5cf1e27d0a87c7666b98138386a094b75c806e50
DharwinRVJ <dharwinruppa1422@gmail.com>
2021-03-30 16:23:36 +0530

    ncm

---
+++ schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE IF NOT EXISTS nodecrud;
+USE nodecrud;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(150) NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `phone_no` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+ALTER TABLE `users` ADD PRIMARY KEY (`id`);
+ALTER TABLE `users` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
